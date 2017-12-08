3c645c81ee1fd3b9c0f5a68a1b4e6a830281cb60
sagar <snkumar.nayak@gmail.com>
2017-12-08 17:18:31 +0530

    commit

--- app/src/main/java/com/sagar/android_projects/androidarch/di/component/AppComponent.java
+++ app/src/main/java/com/sagar/android_projects/androidarch/di/component/AppComponent.java
@@ -1,7 +1,12 @@
 package com.sagar.android_projects.androidarch.di.component;
 
+import com.sagar.android_projects.androidarch.di.scope.AppScope;
+import com.sagar.android_projects.androidarch.repository.AndroidArchRepository;
+
 import dagger.Component;
 
+@AppScope
 @Component
 public interface AppComponent {
+    public AndroidArchRepository androidArchRepository();
 }
