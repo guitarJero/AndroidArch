2fdbacfa24fd934b5d3c45485633c21d2646a748
sagar <snkumar.nayak@gmail.com>
2017-12-18 17:58:50 +0530

    commit

--- app/src/main/java/com/sagar/android_projects/androidarch/core/Const.java
+++ app/src/main/java/com/sagar/android_projects/androidarch/core/Const.java
@@ -2,6 +2,5 @@ package com.sagar.android_projects.androidarch.core;
 
 
 public class Const {
-    public static final String LOG_TAG="Android_Arch_Log";
     public static final String BASE_URL="https://reqres.in/";
 }
--- app/src/main/java/com/sagar/android_projects/androidarch/util/LogUtil.java
+++ app/src/main/java/com/sagar/android_projects/androidarch/util/LogUtil.java
@@ -4,36 +4,37 @@ package com.sagar.android_projects.androidarch.util;
 import android.util.Log;
 
 import com.sagar.android_projects.androidarch.BuildConfig;
-import com.sagar.android_projects.androidarch.core.Const;
 
 public class LogUtil {
+    private static final String LOG_TAG = "Android_Arch_Log";
+
     @SuppressWarnings("unused")
     public static void logV(String message) {
         if (BuildConfig.DEBUG)
-            Log.v(Const.LOG_TAG, message);
+            Log.v(LOG_TAG, message);
     }
 
     @SuppressWarnings("unused")
     public static void logD(String message) {
         if (BuildConfig.DEBUG)
-            Log.d(Const.LOG_TAG, message);
+            Log.d(LOG_TAG, message);
     }
 
     @SuppressWarnings("unused")
     public static void logI(String message) {
         if (BuildConfig.DEBUG)
-            Log.i(Const.LOG_TAG, message);
+            Log.i(LOG_TAG, message);
     }
 
     @SuppressWarnings("unused")
     public static void logW(String message) {
         if (BuildConfig.DEBUG)
-            Log.w(Const.LOG_TAG, message);
+            Log.w(LOG_TAG, message);
     }
 
     @SuppressWarnings("unused")
     public static void logE(String message) {
         if (BuildConfig.DEBUG)
-            Log.e(Const.LOG_TAG, message);
+            Log.e(LOG_TAG, message);
     }
 }
