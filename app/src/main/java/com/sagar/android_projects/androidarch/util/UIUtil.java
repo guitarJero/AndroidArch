0cc0076312c105266d7def741bcd01bd5bbc09f7
sagar <snkumar.nayak@gmail.com>
2018-01-01 11:18:06 +0530

    commit

--- app/src/main/java/com/sagar/android_projects/androidarch/util/UIUtil.java
+++ app/src/main/java/com/sagar/android_projects/androidarch/util/UIUtil.java
@@ -1,19 +1,47 @@
 package com.sagar.android_projects.androidarch.util;
 
-
+import android.app.Activity;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
+/**
+ * created by SAGAR KUMAR NAYAK
+ * util class to perform any kind of UI operations
+ * <p>
+ * exposed methods
+ * {@link #hideSoftKeyboard(Context)}
+ * {@link #hideSoftKeyboardAtStart(Context)}
+ */
 public class UIUtil {
+
+    /**
+     * method to hide the soft keyboard
+     *
+     * @param context context
+     */
+    @SuppressWarnings({"unused", "SameParameterValue", "WeakerAccess"})
     public static void hideSoftKeyboard(Context context) {
-        View view = ((AppCompatActivity) context).getCurrentFocus();
+        View view = ((Activity) context).getCurrentFocus();
         if (view != null) {
-            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm =
+                    (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
             if (imm == null)
                 return;
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+    }
+
+    /**
+     * method to set the activity to not to show the soft keyboard at activity start.
+     * to use this method call it before {@link Activity#setContentView(int)} in Activity
+     *
+     * @param context context
+     */
+    @SuppressWarnings({"unused", "SameParameterValue", "WeakerAccess"})
+    public static void hideSoftKeyboardAtStart(Context context) {
+        ((Activity) context).getWindow().setSoftInputMode(WindowManager.
+                LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
